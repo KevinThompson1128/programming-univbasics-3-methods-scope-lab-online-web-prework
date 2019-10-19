c0666b7b3d320c49de2db4c9c34c7135a37795f1
Kevin Thompson <indelible11@yahoo.com>
2019-10-19 18:01:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,4 +7,9 @@ def toadstool
     status = 'Thank You Mario! But Our Princess Is In Another Castle!'
     puts status
 end
+
 def link 
+  status = "It's Dangerous To Go Alone! Take This."
+  puts status
+end
+
